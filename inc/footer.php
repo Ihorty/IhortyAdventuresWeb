5ea0a7f6ae17d248e64d02caf346a124ee8b2bd0
Igor Tsyshevskyy <igor.rayden@gmail.com>
2021-04-07 17:40:01 +0200

    Beauty updates for main page

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,10 @@
 <footer>
-
+    <div class="credits">
+        <p>Fonts: <a href="https://www.dafont.com/es/oliciy.font">Oliciy</a>, <a href="https://fonts.google.com/specimen/Merriweather+Sans">Merriweather Sans</a></p>
+    </div>
+    <div class="contact">
+        Ihor Tsyshevskyy<br>
+        <a href="igor.tsyshevskyy@gmail.com">igor.tsyshevskyy@gmail.com</a><br>
+        <a href="www.github.com/ihorty">GitHub</a>
+    </div>
 </footer>
