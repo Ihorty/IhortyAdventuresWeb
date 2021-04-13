9cce71726435f6bc937f8e2ebb726e9e4f3c51aa
Igor Tsyshevskyy <igor.rayden@gmail.com>
2021-04-13 17:44:28 +0200

    hotfixing connection

--- inc/ajustes.php
+++ inc/ajustes.php
@@ -1,10 +1,11 @@
 <?php
 
+    /** Hay que dejar descomentado una de las conexiones, si no, va a ir muy lenta la carga de la página */
     //Mi conexion local, cuando el servidor esté subido
-    $conexion = mysqli_connect("192.168.1.128","IhortyReader","R34d3r@PotatoDatabase","ragoria","3307");
+    //$conexion = mysqli_connect("192.168.1.128","IhortyReader","R34d3r@PotatoDatabase","ragoria","3307");
 
     //Mi conexion accedida desde cualquier punto del mundo (permito probar vuestras paginas con mi contenido <3)
-    //$conexion = mysqli_connect("ragoria.com","IhortyReader","R34d3r@PotatoDatabase","ragoria", "3307"); //TODO aun no funciona
+    $conexion = mysqli_connect("ragoria.com","IhortyReader","R34d3r@PotatoDatabase","ragoria", "3307"); 
 
     //Mi conexion local de xampp si todo lo demás falla. Esta conexion fallará si está en mi hosting
     //$conexion = mysqli_connect("localhost","root","","ragoria");
